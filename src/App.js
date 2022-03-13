2f0378d312b41f72689e4eb2d5c1e67588f04471
Laise <laise94@hotmail.com>
2022-03-13 03:04:25 -0300

    corrigindo erro

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
